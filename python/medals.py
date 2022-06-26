a80d66323e61fd72e178e1863bd35139d714e61c
Adam Sackfield <adamsackfield20@gmail.com>
2022-06-26 20:44:39 +0100

    Add comments describing each step

--- python/medals.py
+++ python/medals.py
@@ -18,22 +18,35 @@ medalResults = [
 ]
 
 def createMedalTable(results):
+    # Use the results object above to create a medal table
+    # The winner gets 3 points, second place 2 points and third place 1 point
     output_results = {}
-    
+
+    # Iterate over each sport in the given list
     for sport in results:
+
+      # Iterate over podium list for each sport  
       for podium in sport['podium']:
+        
+        # Get the counrty name, starting at index 2 -> end of string
         country = podium[2:]
+
+        # Get the char at index 0, using int to convert the string to an integar
         position = int(podium[0])
 
+        # Calculate points - since 1=3, 2=2 and 3=1. We can subtract the position from 4 to get the points value required
         points = 4 - position
 
+        # If results does not yet contain the current country we can use the setdefault method to set the value to 0
         output_results.setdefault(country, 0)
+
+        # Then we can increment the current country total using the current points.
         output_results[country] += points
 
     return output_results
 
 def test_function():
-    
+    #This it the test function, please don't change me
     medalTable = createMedalTable(medalResults)
     expectedTable = {
         "Italy": 4,
